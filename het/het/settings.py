1d93900f357ed413c33b7cb8a47560f5da5e7814
Gleb Lazarev <gleb.lazarev20@yandex.ru>
2023-10-30 22:46:05 +0300

    Развернут контейнер для бекенда и бД

--- het/het/settings.py
+++ het/het/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'het.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
